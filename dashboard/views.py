b8495cd54820bd1b1ff559da4edcce9d2fb19b9b
Victor Teixeira <victorteixeira18@outlook.com>
2024-11-06 22:51:38 -0300

    remover_text

--- dashboard/views.py
+++ dashboard/views.py
@@ -3,13 +3,12 @@ from django.db.models import Sum
 from contratos.models import Contrato
 
 def top_contratos_view(request):
-    # Obtenha os 10 contratos com maiores valores
     top_contratos = Contrato.objects.order_by('-valor')[:10]
     
     # Agrupa por categoria e soma os valores para cada categoria
     categoria_gastos = (
         Contrato.objects
-        .values('categoria__nome')  # Supondo que o nome do campo seja 'nome' no modelo Categoria
+        .values('categoria__nome') 
         .annotate(total_valor=Sum('valor'))
         .order_by('-total_valor')[:10]
     )
@@ -17,7 +16,7 @@ def top_contratos_view(request):
     # Agrupa por fornecedor e soma os valores para cada fornecedor
     fornecedor_gastos = (
         Contrato.objects
-        .values('nome_fornecedor__nome')  # Supondo que o campo de fornecedor seja 'nome_fornecedor'
+        .values('nome_fornecedor__nome') 
         .annotate(total_valor=Sum('valor'))
         .order_by('-total_valor')[:10]
     )
